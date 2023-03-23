7d16413ee50b18bc75da06db845ae3ddd96d3e29
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2023-03-23 08:33:09 +0200

    updated hibernate dao & test

---
+++ src/main/java/guru/springframework/jdbc/dao/BookDaoHibernate.java
@@ -0,0 +1,95 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Book;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class BookDaoHibernate implements BookDao {
+    private final EntityManagerFactory emf;
+
+    @Override
+    public List<Book> findAllBooksSortByTitle(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findAllBooks(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findAllBooks(int pageSize, int offset) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findAllBooks() {
+        try (EntityManager em = getEntityManager()) {
+            TypedQuery<Book> query = em.createQuery("SELECT b FROM Book b", Book.class);
+            return query.getResultList();
+        }
+    }
+
+
+    @Override
+    public Book getById(Long id) {
+        try (EntityManager em = getEntityManager()) {
+            return em.find(Book.class, id);
+        }
+    }
+
+    @Override
+    public Book findBookByTitle(String title) {
+        try (EntityManager em = getEntityManager()) {
+            TypedQuery<Book> query = em.createQuery("SELECT b FROM Book b WHERE b.title=:title", Book.class);
+            query.setParameter("title", title);
+
+            return query.getSingleResult();
+        }
+    }
+
+    @Override
+    public Book saveNewBook(Book book) {
+        try (EntityManager em = getEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(book);
+            em.flush();
+            em.getTransaction().commit();
+            em.close();
+
+            return book;
+        }
+    }
+
+    @Override
+    public Book updateBook(Book book) {
+        try (EntityManager em = getEntityManager()) {
+            em.getTransaction().begin();
+            em.merge(book);
+            em.getTransaction().commit();
+
+            return em.find(Book.class, book.getId());
+        }
+    }
+
+    @Override
+    public void deleteBookById(Long id) {
+        try (EntityManager em = getEntityManager()) {
+            em.getTransaction().begin();
+            Book book = em.find(Book.class, id);
+            em.remove(book);
+            em.getTransaction().commit();
+        }
+    }
+
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+}
---
+++ src/test/java/guru/springframework/jdbc/dao/BookDaoHibernateTest.java
@@ -0,0 +1,99 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Book;
+import jakarta.persistence.EntityManagerFactory;
+import lombok.Data;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BookDaoHibernateTest {
+    @Autowired
+    EntityManagerFactory emf;
+
+    BookDao bookDao;
+
+    @BeforeEach
+    void setup() {
+        bookDao  = new BookDaoHibernate(emf);
+    }
+
+    @Test
+    void testFindAllBooks() {
+        List<Book> books = bookDao.findAllBooks();
+
+        assertThat(books).isNotNull();
+        assertThat(books.size()).isGreaterThan(0);
+    }
+
+    @Test
+    void testDeleteBook() {
+        Book book = new Book();
+        book.setIsbn("1234");
+        book.setPublisher("Self");
+        book.setTitle("my book");
+        Book saved = bookDao.saveNewBook(book);
+
+        bookDao.deleteBookById(saved.getId());
+
+        Book deleted = bookDao.getById(saved.getId());
+
+        assertThat(deleted).isNull();
+    }
+
+    @Test
+    void updateBookTest() {
+        Book book = new Book();
+        book.setIsbn("1234");
+        book.setPublisher("Self");
+        book.setTitle("my book");
+
+        book.setAuthorId(3L);
+        Book saved = bookDao.saveNewBook(book);
+
+        saved.setTitle("New Book");
+        bookDao.updateBook(saved);
+
+        Book fetched = bookDao.getById(saved.getId());
+
+        assertThat(fetched.getTitle()).isEqualTo("New Book");
+    }
+
+    @Test
+    void testSaveBook() {
+        Book book = new Book();
+        book.setIsbn("1234");
+        book.setPublisher("Self");
+        book.setTitle("my book");
+
+        book.setAuthorId(3L);
+        Book saved = bookDao.saveNewBook(book);
+
+        assertThat(saved.getId()).isNotNull();
+    }
+
+    @Test
+    void testGetBookByName() {
+        Book book = bookDao.findBookByTitle("Clean Code");
+
+        assertThat(book).isNotNull();
+    }
+
+    @Test
+    void testGetBook() {
+        Book book = bookDao.getById(3L);
+
+        assertThat(book.getId()).isNotNull();
+    }
+}
